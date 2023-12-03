13d71dc38f7e92f605e3edc01694916ff406430e
Thijs Olijerhoek <t.g.a.olijerhoek@students.uu.nl>
2023-12-03 11:50:13 +0100

    should be right, idk what's going wrong

--- Day2/d2.hs
+++ Day2/d2.hs
@@ -4,15 +4,16 @@ import Data.Char (digitToInt)
 
 main :: IO ()
 main = do
-    fileContent <- readFile "inp.txt"
+    fileContent <- readFile "input.txt"
     --this is all lines in the file as one array entry.
     let color = emptyColor
     let inputArray = lines fileContent
-    let splittedOnSemicolon =  map (splitOn ";") inputArray -- :t [[String]] == [[[Char]]]
-    let c = addToColor color "5 red, 3 blue, 11 green" 
-    let resColor = map (addToColor color) $ head splittedOnSemicolon
-    print c
+    let splittedOnSemicolon =  map (splitOn "; ") inputArray -- :t [[String]] == [[[Char]]] --> dit zijn alle lines, ofwel alle games.
+    let allColors = map (sumColors . makeAllColors emptyColor) splittedOnSemicolon
+    let validColors = checkValidColor allColors
+    print $ sumIndexesOfTrue validColors 
 
+-- add up the ID's of valid games
 -- 12 red cubes, 13 green cubes, and 14 blue cubes
 -- each game gets a color count. then we can compare said color to the required values
 -- 1 entry of 5 sets: ["1 red, 3 blue, 11 green"," 1 blue, 5 red"," 3 blue, 5 green, 13 red"," 6 red, 1 blue, 4 green"," 16 red, 12 green"]
@@ -20,18 +21,38 @@ data Color = Color{red :: Int, green :: Int , blue :: Int} deriving Show
 emptyColor :: Color
 emptyColor = Color 0 0 0
 
-{-sum a list of colors-}
-{-sum two colors-}
+addColors :: Color -> Color -> Color
+addColors (Color r1 g1 b1) (Color r2 g2 b2) = Color (r1+r2) (g1+g2) (b1+b2)
+
+sumColors :: [Color] -> Color
+sumColors = foldr addColors (Color 0 0 0)
+
+-- getOneEntry :: Color -> [Color]
+-- getOneEntry color
+
+checkValidColor :: [Color] -> [Bool]
+checkValidColor = map (\x -> red x < 13 && green x < 14 && blue x < 15)
+{- same semantics, just haskell reduce
+checkValidColor [] = []
+checkValidColor (x:xs) = (red x < 13 && green x < 14 && blue x < 15) : checkValidColor xs
+-}
+
+sumIDs :: [Bool] -> Int -> Int
+sumIDs [] _ = 0
+sumIDs (x:xs) id = if x then id + sumIDs xs id+1 else sumIDs xs id+1
+
+sumIndexesOfTrue :: [Bool] -> Int
+sumIndexesOfTrue boolList = sum [index | (value, index) <- zip boolList [1..], value]
 
 -- given a color and a character array, make for each encountered color
 -- given a single set, like "1 red, 3 blue, 11 green", return a new color that has the added amounts
 -- map dit over 1 entry
-makeColorFromEntry :: [String] -> Color -> [Color]
-makeColorFromEntry [] c = [c]
-makeColorFromEntry str@(x:xs) c = map (addToColor c) str 
+makeAllColors :: Color -> [String] -> [Color]
+makeAllColors _ [] = []
+makeAllColors color (x:xs) = addToColor color x : makeAllColors color xs
 
 addToColor :: Color -> String -> Color
-addToColor color str = makeColor color scores 
+addToColor color str = makeColor color scores
     where
         a@(x:xs) = splitOn ", " str -- ["1 red", "3 blue", "11 green"]
         scores = map checkAmount a --scores: [(1,"red"),(3,"blue"),(11,"green")]
@@ -43,18 +64,16 @@ addToColor color str = makeColor color scores
 --convert a 
 checkAmount :: String -> (Int, String)
 checkAmount str
-    | 'g' `elem` str = (read (head $ splitOn " " str), "green") 
+    | 'g' `elem` str = (read (head $ splitOn " " str), "green")
     | 'b' `elem` str = (read (head $ splitOn " " str), "blue")
     | otherwise = (read (head $ splitOn " " str), "red")
-
 
 --read the string and the int value, 
 -- map makeColor (Array of )
 makeColor ::  Color -> [(Int, String)] -> Color
 makeColor c [] = c
-makeColor  Color{red = r, green = g, blue = b } (x:xs) = 
+makeColor  Color{red = r, green = g, blue = b } (x:xs) =
     case snd x of
-        "red"  -> makeColor (Color (r+1) g b) xs
-        "blue" -> makeColor (Color r g (b+1)) xs
-        "green" -> makeColor (Color r (g+1) b) xs
---Read one element, find out what the value is and to what color it matches
+        "red"  -> makeColor (Color (r + fst x) g b) xs
+        "blue" -> makeColor (Color r g (b + fst x)) xs
+        "green" -> makeColor (Color r (g + fst x) b) xs
